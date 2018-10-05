6ad316e4e29419068ca0da97d4da4533ec7b7b94
Jon Šotola <maw00x@gmail.com>
2018-10-05 20:29:27 +0200

    Update check_delete.py

--- check_delete.py
+++ check_delete.py
@@ -2,6 +2,8 @@ from ftplib import FTP_TLS, error_perm
 from time import sleep
 import socket
 from collections import defaultdict
+import logging
+import logging.handlers
 
 
 MAX_SIZE = 5  # GB
@@ -14,10 +16,33 @@ ARMED = True
 VERBOSE = False
 
 
-with open("secrets", "r") as f:
-    secrets = f.read()
+LOG_FILENAME = "log.out"
 
-host, user, passwd = secrets.split()
+
+def set_up_logging():
+
+    # Set up a specific logger with our desired output level
+    my_logger = logging.getLogger("MyLogger")
+    my_logger.setLevel(logging.DEBUG)
+
+    # Add the log message handler to the logger
+    handler = logging.handlers.RotatingFileHandler(
+        LOG_FILENAME, maxBytes=1024 * 100, backupCount=1
+    )
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    handler.setFormatter(formatter)
+
+    my_logger.addHandler(handler)
+
+
+def load_secrets():
+    global host, user, passwd
+    with open("secrets", "r") as f:
+        secrets = f.read()
+
+    host, user, passwd = secrets.split()
 
 
 def to_MB(size_in_bytes):
@@ -53,8 +78,8 @@ def get_all_files(ftp):
 
         dict_directories[directory] = files_names
 
-        print(directory, len(files), "files,", to_MB(s), "MB", end="     ")
-        print("Data from:", str(dates_dict)[27:-1])
+        my_logger.info(f"{directory}{len(files)} files {to_MB(s)} MB")
+        my_logger.info(f"Data from: {str(dates_dict)[27:-1]}")
 
     return file_list, dict_directories
 
@@ -63,7 +88,7 @@ def delete_old_files(ftp, files, size_to_delete, count_to_delete):
     file_keys = list(files.keys())
     file_keys.sort(key=lambda x: x.split("/")[1])
     deleted_size, deleted_n = 0, 0
-    print("Oldest file is", file_keys[0], "and newest file is", file_keys[-1], ".")
+    my_logger.info(f"Oldest file is {file_keys[0]} and newest file is {file_keys[-1]}.")
     while deleted_size < size_to_delete or deleted_n < count_to_delete:
         file_to_delete = file_keys[0]
         deleted_size += files.pop(file_to_delete)
@@ -87,50 +112,59 @@ def delete_old_in_dir(ftp, files, directory, count_to_delete):
 
 
 def ftp_check_size(run_counter=0):
-    run_counter +=1
+    run_counter += 1
     try:
         with FTP_TLS(host=host, user=user, passwd=passwd) as ftp:
-            print("Connected. Looking for files.")
+            my_logger.info("Connected. Looking for files.")
             file_dict, dir_dict = get_all_files(ftp)
 
-            print("\nChecking file counts in directories.")
+            my_logger.info("Checking file counts in directories.")
             for directory, files in dir_dict.items():
-                print(directory, "has", len(files), "files.", end=" ")
+                my_logger.info(f"{directory} has {len(files)} files.")
                 if MAX_DIR_COUNT < len(files):
-                    print("That is", len(files) - MAX_DIR_COUNT, "too too many. Deleting...", end=" ")
-                    deleted_n = delete_old_in_dir(ftp, files, directory, len(files) - MAX_DIR_COUNT)
-                    print("Deleted", deleted_n, end=" ")
-                print("")
+                    my_logger.info(
+                        f"That is {len(files) - MAX_DIR_COUNT} too too many. Deleting..."
+                    )
+                    deleted_n = delete_old_in_dir(
+                        ftp, files, directory, len(files) - MAX_DIR_COUNT
+                    )
+                    my_logger.info(f"Deleted {deleted_n}")
+                my_logger.info("")
 
-            print("\nChecking total size and count.")
+            my_logger.info("Checking total size and count.")
             total_size = sum(file_dict.values())
-            print("In total", len(file_dict), "files,", to_MB(total_size), "MB. ")
-            size_over_limit = total_size - MAX_SIZE * 1024 * 1024 * 1024 * (1 - KEEP_RESERVE)
+            my_logger.info(f"In total {len(file_dict)} files {to_MB(total_size)}MB.")
+            size_over_limit = total_size - MAX_SIZE * 1024 * 1024 * 1024 * (
+                1 - KEEP_RESERVE
+            )
             count_over_limit = len(file_dict) - MAX_FILE_COUNT * (1 - KEEP_RESERVE)
             if size_over_limit > 0 or count_over_limit > 0:
-                print("Too big or too many files. Deleting files...")
-                deleted_size, deleted_n = delete_old_files(ftp, file_dict, size_over_limit, count_over_limit)
-                print("Deleted", deleted_n, " files", to_MB(deleted_size), "MB in total. Current size is",
-                      to_MB(total_size))
+                my_logger.info("Too big or too many files. Deleting files...")
+                deleted_size, deleted_n = delete_old_files(
+                    ftp, file_dict, size_over_limit, count_over_limit
+                )
+                my_logger.info(
+                    f"Deleted {deleted_n} files {to_MB(deleted_size)} MB in total. Current size is {to_MB(total_size)}"
+                )
             else:
-                print("Size is fine.")
-
+                my_logger.info("Size is fine.")
 
     except (ConnectionAbortedError, error_perm) as e:
-        print(e)
-        print("Connection aborted. Restarting...")
+        my_logger.error(e)
+        my_logger.error("Connection aborted. Restarting...")
         if run_counter < 100:
             ftp_check_size(run_counter=run_counter)
 
-
     except socket.gaierror as e:
-        print(e)
-        print("Connection failed.")
+        my_logger.error(e)
+        my_logger.error("Connection failed.")
 
-while __name__ == "__main__":
+
+if __name__ == "__main__":
+    set_up_logging()
+    my_logger.info("")
+    my_logger.info("Starting..")
+    load_secrets()
     ftp_check_size()
-    if RUN_ONLY_ONCE:
-        print("\nFinished.")
-        break
-    print("\nWill check again in ", CHECK_EVERY, "hour(s).")
-    sleep(CHECK_EVERY * 3.6 * 10 ** 6)
+
+    my_logger.info("Finished.")
